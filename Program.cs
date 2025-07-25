2d5cb2b06c517da0e13307e8690886cd9ffe1f50
AngelaRebancosBautista <angelarbautista22@gmail.com>
2025-07-25 12:55:20 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratory_Activity_31
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a single word: ");
+            string word = Console.ReadLine();
+
+            if (IsPalindrome(word))
+            {
+                Console.WriteLine("Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Not a palindrome");
+            }
+        }
+        static bool IsPalindrome(string input)
+        {
+            int left = 0; 
+            int right = input.Length - 1; 
+
+            while (left < right)
+            {
+                if (input[left] != input[right])
+                {
+                    return false; 
+                }
+                left++; 
+                right--; 
+            }
+            return true; 
+        }
+    }
+}
